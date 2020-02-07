104d73799f61a3ca8115c986004e4e208616f82c
ber01 <ber01@naver.com>
2020-02-08 01:52:12 +0900

    Added PUT "/todos/{id}" 테스트 코드 작성
    - completed 변경에 따른 Todo 객체 생성 테스트 수정
    - completed 변경에 따른 각종 테스트 작성

--- src/test/java/me/kyunghwan/todos/todo/TodoApiControllerTest.java
+++ src/test/java/me/kyunghwan/todos/todo/TodoApiControllerTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Description;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -87,6 +89,95 @@ public class TodoApiControllerTest {
         assertThat(target.getId()).isEqualTo(todo.getId());
         assertThat(target.getName()).isEqualTo(name);
         assertThat(target.isCompleted()).isEqualTo(false);
+    }
+
+    @Test
+    @Description("Todo를 등록했을 때 completed가 true면 completedAt의 값이 존재하는 테스트")
+    public void todoCreateWithCompleteTrue() {
+        // given
+        String name = "name";
+        TodoRequestDto requestDto = TodoRequestDto.builder()
+                .name(name)
+                .completed(true)
+                .build();
+
+        String url = "http://localhost:" + port + "/todos";
+
+        // when
+        ResponseEntity<TodoResponseDto> responseEntity = testRestTemplate.postForEntity(url, requestDto, TodoResponseDto.class);
+
+        // then
+        Todo todo = todoRepository.findById(responseEntity.getBody().getId()).get();
+        assertThat(todo.isCompleted()).isTrue();
+        assertThat(todo.getCompleteAt()).isNotNull();
+    }
+
+    @Test
+    @Description("Todo를 수정하는 테스트 complete가 false -> true 수정될 때, completeAt 값이 존재")
+    public void todoUpdateTest() {
+        // given
+        Todo todo = todoRepository.save(Todo.builder()
+                .name("name")
+                .completed(false)
+                .build());
+
+        Integer updateId = todo.getId();
+        String updateName = "Update_name";
+
+        TodoRequestDto requestDto = TodoRequestDto.builder()
+                .name(updateName)
+                .completed(true)
+                .build();
+
+        String url = "http://localhost:" + port + "/todos/" + updateId;
+
+        HttpEntity<TodoRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<TodoResponseDto> responseEntity = testRestTemplate.exchange(url, HttpMethod.PUT, requestEntity, TodoResponseDto.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        Todo updateTodo = todoRepository.findById(updateId).get();
+        assertThat(updateTodo.getName()).isEqualTo(updateName);
+        assertThat(updateTodo.isCompleted()).isTrue();
+        assertThat(updateTodo.getCompleteAt()).isNotNull();
+        assertThat(updateTodo.getUpdatedAt()).isAfter(updateTodo.getCreatedAt());
+    }
+
+    @Test
+    @Description("Todo를 수정하는 테스트 complete가 true -> false 수정될 때, completeAt가 null")
+    public void todoUpdateTest2() {
+        // given
+        Todo todo = todoRepository.save(Todo.builder()
+                .name("name")
+                .completed(true)
+                .build());
+
+        Integer updateId = todo.getId();
+        String updateName = "Update_name";
+
+        TodoRequestDto requestDto = TodoRequestDto.builder()
+                .name(updateName)
+                .completed(false)
+                .build();
+
+        String url = "http://localhost:" + port + "/todos/" + updateId;
+
+        HttpEntity<TodoRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<TodoResponseDto> responseEntity = testRestTemplate.exchange(url, HttpMethod.PUT, requestEntity, TodoResponseDto.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        Todo updateTodo = todoRepository.findById(updateId).get();
+        assertThat(updateTodo.getName()).isEqualTo(updateName);
+        assertThat(updateTodo.isCompleted()).isFalse();
+        assertThat(updateTodo.getCompleteAt()).isNull();
+        assertThat(updateTodo.getUpdatedAt()).isAfter(updateTodo.getCreatedAt());
     }
 
 }
--- src/test/java/me/kyunghwan/todos/todo/TodoTest.java
+++ src/test/java/me/kyunghwan/todos/todo/TodoTest.java
@@ -3,15 +3,13 @@ package me.kyunghwan.todos.todo;
 import org.junit.Test;
 import org.springframework.context.annotation.Description;
 
-import java.time.LocalDateTime;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TodoTest {
 
     @Test
-    @Description("객체 생성 테스트")
-    public void test() {
+    @Description("객체 생성 테스트, completed가 false면 complteAt값 null")
+    public void newTodoCompleteFalse() {
         String name = "name";
 
         Todo todo = Todo.builder()
@@ -21,6 +19,22 @@ public class TodoTest {
 
         assertThat(todo).isNotNull();
         assertThat(todo.getName()).isEqualTo(name);
+        assertThat(todo.getCompleteAt()).isNull();
+    }
+
+    @Test
+    @Description("객체 생성 테스트, completed가 false면 complteAt값 존재")
+    public void newTodoCompleteTrue() {
+        String name = "name";
+
+        Todo todo = Todo.builder()
+                .name(name)
+                .completed(true)
+                .build();
+
+        assertThat(todo).isNotNull();
+        assertThat(todo.getName()).isEqualTo(name);
+        assertThat(todo.getCompleteAt()).isNotNull();
     }
 
 }
