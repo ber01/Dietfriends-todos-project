ab20a4cefcf9158e39be4bd5ee10cd4cff6e07bc
ber01 <ber01@naver.com>
2020-02-09 06:48:04 +0900

    Added AuthServerConfig
    - 인증 서버 설정
    - 인증 토큰 발급 테스트

---
+++ src/test/java/me/kyunghwan/todos/config/AuthServerConfigTest.java
@@ -0,0 +1,60 @@
+package me.kyunghwan.todos.config;
+
+import me.kyunghwan.todos.user.User;
+import me.kyunghwan.todos.user.UserRole;
+import me.kyunghwan.todos.user.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Description;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AuthServerConfigTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    @Description("인증 토큰을 발급받는 테스트")
+    public void getAuthToken() throws Exception {
+        // given
+        String username = "email@email.com";
+        String password = "password";
+
+        User user = User.builder()
+                .email(username)
+                .password(password)
+                .roles(UserRole.USER)
+                .build();
+        this.userService.saveUser(user);
+
+        String clientId = "diet";
+        String clientSecret = "friends";
+
+        // when & then
+        this.mockMvc.perform(post("/oauth/token")
+                .with(httpBasic(clientId, clientSecret))
+                .param("username", username)
+                .param("password", password)
+                .param("grant_type", "password"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("access_token").exists());
+    }
+
+}
